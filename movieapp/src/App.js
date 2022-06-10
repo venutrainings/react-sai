7cc2d3f670883dc814f60e7ab35c03db8ffeca75
pusamsaikuamr <pusamsaikumar302@gmail.com>
2022-06-10 09:18:31 -0700

    files added

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,26 +1,76 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
   const [movies,setMovies]=useState([]);
   const [searchValue,setSearchValue] = useState('');
+  const [favourites,setFavourites] = useState([]);
+
+  const saveToLocalStorage = (items) =>{
+    localStorage.setItem('react-app-movie-favourites',items)
+  }
+
+  const hadleAddFavourite=(movie)=>{
+    const newAddFavourite = [...favourites,movie];
+    setFavourites(newAddFavourite);
+    saveToLocalStorage(newAddFavourite);
+  }
+
   
+
+  const hadleRemoveFavourite=(movie)=>{
+    const newRemoveFavourite=favourites.filter((favourite)=> favourite.omdbID !== movie.omdbID);
+    setFavourites(newRemoveFavourite);
+    saveToLocalStorage(newRemoveFavourite);
+  }
+
+  const getMoviesRequest = async (searchValue) =>{
+const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=883b0af1`;
  
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson)
+ if(responseJson.Search){
+   setMovies(responseJson.Search);
+ }
+  }
+ useEffect(()=>{
+   getMoviesRequest(searchValue)
+ },[searchValue]);
   return (
    <div className='container-fluid movies-app' >
         <div className='row d-flex align-items-center mt-4 mb-4'>
-          <MovieListHeading heading='Movies' />
-          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
+            <MovieListHeading heading='Movies' />
+            <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        </div>
 
       <div className="row">
-        <MovieList movies={movies} />
+            <MovieList 
+               movies={movies} 
+                hadleFavourite={hadleAddFavourite}
+                 AddFavourites={AddFavourites}
+             />
       </div>
+   
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+            <MovieListHeading heading='Favourites' />
+            
+        </div>
+        <div className="row">
+            <MovieList 
+               movies={favourites} 
+               hadleFavourite = {hadleRemoveFavourite}
+               RemoveFavourites={RemoveFavourites}
+
+            />
+      </div> 
   </div>
   );
 }
--- movieapp/src/components/MovieList.js
+++ movieapp/src/components/MovieList.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-function MovieList({movies}) {
+
+function MovieList({movies, hadleFavourite,AddFavourites,RemoveFavourites,}) {
   return (
-    <div>
+    
         <>
         {
             movies.map((movie,index)=>{
-                return <div key={index}>
-                    <h4>{movie.Title}</h4>
-                    <img src={movie.Poster} />
+                return <div className='image-container d-flex justify-content-start m-3' key={index}>
+                        <img src={movie.Poster} alt='movieposter' />
+
+                        <div 
+                            className='overlay d-flex align-items-center justify-content-center'
+                            onClick={()=>hadleFavourite(movie)}>
+                             <AddFavourites />
+                             {/* <RemoveFavourites />  */}
+                        </div>
+                        
                 </div>
             })
         }
         </>
-    </div>
+    
   )
 }
 
